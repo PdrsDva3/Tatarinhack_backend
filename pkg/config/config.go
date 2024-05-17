1d08a0cafaea5aa6b5baa01a4d0f497c21ef6f75
Sve4ka <aria_stark_two@mail.ru>
2024-05-17 19:12:05 +0300

    first commit

---
+++ pkg/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const (
+	DBName     = "DB_NAME"
+	DBUser     = "DB_USER"
+	DBPassword = "DB_PASSWORD"
+	DBHost     = "DB_HOST"
+	DBPort     = "DB_PORT"
+	TimeOut    = "DB_TIMEOUT"
+)
+
+func InitConfig() {
+	path, _ := os.Getwd()
+
+	path = filepath.Join(path, "..")
+	path = filepath.Join(path, "/deploy")
+
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+
+	//fmt.Println(viper.GetString(DBHost))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Sprintf("Config initialization error: %v", err.Error()))
+	}
+}
